4845984831aee9699d4b0c3d26fc1728dc1d6024
Robert F. Dickerson <rfdickerson@gmail.com>
2015-12-10 08:20:51 -0600

    Added a Facebook based OAuth client

---
+++ client/WatsonSDKTokenFetch/WatsonSDKTokenFetch/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  WatsonSDKTokenFetch-Bridging-Header.h
+//  WatsonSDKTokenFetch
+//
+//  Created by Robert Dickerson on 12/8/15.
+//  Copyright © 2015 Robert Dickerson. All rights reserved.
+//
+
+#ifndef WatsonSDKTokenFetch_Bridging_Header_h
+#define WatsonSDKTokenFetch_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif /* WatsonSDKTokenFetch_Bridging_Header_h */
---
+++ client/WatsonSDKTokenFetch/WatsonSDKTokenFetch/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  WatsonSDKTokenFetch
+//
+//  Created by Robert Dickerson on 12/8/15.
+//  Copyright © 2015 Robert Dickerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    @IBOutlet weak var tokenLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+                // User already logged in.
+            
+            print("User already logged in with token \(FBSDKAccessToken.currentAccessToken().tokenString)")
+            
+            tokenLabel.text = FBSDKAccessToken.currentAccessToken().tokenString
+            
+        } else { }
+        
+        let loginView: FBSDKLoginButton = FBSDKLoginButton()
+        
+        self.view.addSubview(loginView)
+        loginView.center = self.view.center
+        loginView.readPermissions = ["public_profile", "email"]
+        loginView.delegate = self
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        let token = FBSDKAccessToken.currentAccessToken()
+        print("User Logged In token \(token): ")
+        
+        tokenLabel.text = FBSDKAccessToken.currentAccessToken().tokenString
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+        
+        tokenLabel.text = ""
+    }
+
+
+}
+
